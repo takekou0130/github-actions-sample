542fb72fa2b9ec92d2272c1ea6963820d9d29cf4
takekou0130 <60650456+takekou0130@users.noreply.github.com>
2023-02-14 01:23:47 +0900

    update

--- .github/post.sh
+++ .github/post.sh
@@ -7,7 +7,11 @@ cat <<EOF
 EOF
 }
 
-echo payload
-response=$(curl -s -X POST --data-urlencode "payload=$(payload)" "https://www.google.com")
+response=$(curl -s -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN} \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/6" \
+  -X PATCH
+  -d "$(payload)")
 
 echo $response
