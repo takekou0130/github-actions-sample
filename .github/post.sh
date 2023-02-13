6b1115210f89edc0112142f4ec55184a9fbf81b3
takekou0130 <60650456+takekou0130@users.noreply.github.com>
2023-02-14 01:45:55 +0900

    update

--- .github/post.sh
+++ .github/post.sh
@@ -11,7 +11,7 @@ response=$(curl -s -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN} \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/6" \
-  -X PATCH
+  -X PATCH \
   --data-urlencode "payload=$(payload)")
 
 echo $response
