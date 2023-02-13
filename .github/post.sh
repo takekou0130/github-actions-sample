e9cfe95837684089cec2af7225de523040f2a86e
takekou0130 <60650456+takekou0130@users.noreply.github.com>
2023-02-14 01:44:03 +0900

    update

--- .github/post.sh
+++ .github/post.sh
@@ -12,6 +12,6 @@ response=$(curl -s -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/6" \
   -X PATCH
-  -d "$(payload)")
+  --data-urlencode "payload=$(payload)")
 
 echo $response
