7254535d7b1296eefcd43d8fe9a119299ba82991
takekou0130 <60650456+takekou0130@users.noreply.github.com>
2023-02-14 01:57:42 +0900

    update

--- .github/post.sh
+++ .github/post.sh
@@ -1,14 +1,14 @@
 payload()
 {
-cat <<EOS
+cat <<EOF
 {
   "text": "$MESSAGE"
 }
-EOS
+EOF
 }
 
 response=$(curl -s -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GITHUB_TOKEN} \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/6" \
   -X PATCH \
