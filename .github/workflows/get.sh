18ef215407c18d27227c920e7678d177cc02fcf2
takekou0130 <60650456+takekou0130@users.noreply.github.com>
2023-02-14 00:08:46 +0900

    update

---
+++ .github/workflows/get.sh
@@ -0,0 +1,4 @@
+curl -sSL -X GET -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/branches/develop"
